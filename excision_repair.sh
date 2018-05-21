ddc358d8f47c85218d384bf5404ebfb5b0c5e90d
Yuchao Jiang <yuchaojiang@users.noreply.github.com>
2018-05-21 14:43:07 -0400

    Update excision_repair.sh

--- excision_repair.sh
+++ excision_repair.sh
@@ -6,3 +6,7 @@ bwa aln -t 16 Mus_musculus.GRCm38.dna.primary_assembly.fa sample.cu.fastq > samp
 bwa samse Mus_musculus.GRCm38.dna.primary_assembly.fa sample.cu.sai sample.cu.fastq > sample.cu.sam
 
 # Sam to bam, then sort, dedup, index
+samtools view -bS sample.cu.sam > sample.cu.bam
+java -jar picard.jar SortSam I=sample.cu.bam O=sample.cu.sorted.bam SORT_ORDER=coordinate
+java -jar picard.jar MarkDuplicates I=sample.cu.sorted.bam O=sample.cu.sorted.dedup.bam  M=sample.cu.sorted.dedup.metrics.txt
+java -jar picard.jar BuildBamIndex I=sample.cu.sorted.dedup.bam
