016539b420ab06a1ffae4c74a33cb988e96bff9f
Yuchao Jiang <yuchaojiang@users.noreply.github.com>
2018-05-21 14:41:26 -0400

    Update excision_repair.sh

--- excision_repair.sh
+++ excision_repair.sh
@@ -1,1 +1,8 @@
-# Trim 3' end adaptor sequence and discard untrimmed ones
+# Trim 3' end adaptor sequence and discard short (<= 10bp and untrimmed reads
+cutadapt -a TGGAATTCTCGGGTGCCAAGGAACTCCAGTNNNNNNACGATCTCGTATGCCGTCTTCTGCTTG --discard-untrimmed -m 10 -o sample.cu.fastq sample.fastq
+
+# Align using BWA (single-end sequencing, read length 50bp)
+bwa aln -t 16 Mus_musculus.GRCm38.dna.primary_assembly.fa sample.cu.fastq > sample.cu.sai
+bwa samse Mus_musculus.GRCm38.dna.primary_assembly.fa sample.cu.sai sample.cu.fastq > sample.cu.sam
+
+# Sam to bam, then sort, dedup, index
