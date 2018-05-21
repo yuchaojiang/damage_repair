d1bf974c253f521ad8982ad0373e5c0d4a96d2f5
Yuchao Jiang <yuchaojiang@users.noreply.github.com>
2018-05-21 14:53:41 -0400

    Update RNA_seq.sh

--- RNA_seq.sh
+++ RNA_seq.sh
@@ -1,4 +1,16 @@
-# Generate genome
+# Generate genome (read length 50bp)
+STAR --runThreadN 4 --runMode genomeGenerate --genomeDir Mus_STAR --genomeFastaFiles Mus_musculus.GRCm38.dna.primary_assembly.fa --sjdbGTFfile Mus_musculus.GRCm38.91.gtf --sjdbOverhang 49
 
-# STAR alignment
+# STAR alignment (paired-end RNA sequencing)
+STAR --genomeDir Mus_STAR --readFilesIn sample_R1.fastq sample_R2.fastq --outFilterIntronMotifs RemoveNoncanonicalUnannotated –runThreadN 2 --outFileNamePrefix sample_
 
+
+# Sam to bam, filter, and sort
+# filter_sam_v2.pl available in this GitHub page
+samtools view -bS sample_Aligned.out.sam > sample_Aligned.out.bam
+perl filter_sam_v2.pl sample_Aligned.out.bam sample_Aligned.out.filtered.sam
+samtools view -bS sample_Aligned.out.filtered.sam > sample_Aligned.out.filtered.bam
+java -Xmx30G -jar picard.jar SortSam INPUT=sample_Aligned.out.filtered.bam OUTPUT=sample_Aligned.out.filtered.sorted.bam SORT_ORDER=coordinate
+
+# Build index
+java -jar picard.jar BuildBamIndex I=sample_Aligned.out.filtered.sorted.bam
