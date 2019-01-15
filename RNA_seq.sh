d0ed1a504dd0ad6d5e016b4d52ca99b7b5bad63b
Yuchao Jiang <yuchaojiang@users.noreply.github.com>
2019-01-14 23:29:52 -0500

    Update RNA_seq.sh

--- RNA_seq.sh
+++ RNA_seq.sh
@@ -17,3 +17,6 @@ java -jar picard.jar BuildBamIndex I=sample_Aligned.out.filtered.sorted.bam
 
 # Use featurecounts to get read counts across all genes
 featureCounts -t exon -g gene_id -a Mus_musculus.GRCm38.91.gtf -o sample.featurecounts.txt sample_Aligned.out.filtered.sorted.bam" 
+
+# Use SALMON to get transcript per million matrix (assembly free)
+salmon quant -i transcripts_index -l A -1 sample_R1.fastq -2 sample_R2.fastq -o sample
