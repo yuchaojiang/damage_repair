6fbc29e60ddaaa7f3aadc915013eb777891d0dcf
Yuchao Jiang <yuchaojiang@users.noreply.github.com>
2023-09-13 13:21:10 -0500

    Update 3_XR_RNA.R

--- XPC_C_elegans/3_XR_RNA.R
+++ XPC_C_elegans/3_XR_RNA.R
@@ -1,4 +1,3 @@
-setwd("~/Dropbox/Sancar/Cansu_new/scripts/")
 
 library(Rsamtools)
 library(BSgenome.Celegans.UCSC.ce11)
@@ -157,8 +156,6 @@ rm(count.mat2)
 
 save.image(file='XR_RNA.rda')
 
-
-setwd("~/Dropbox/Sancar/Cansu_new/scripts/")
 
 library(Rsamtools)
 library(BSgenome.Celegans.UCSC.ce11)
